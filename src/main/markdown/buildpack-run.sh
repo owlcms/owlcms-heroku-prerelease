572bf9dc1a244b058dc98b070868224ee7003b26
Jean-François Lamy <jf@jflamy.dev>
2020-02-03 14:14:04 -0500

    get correct uber-jar from bintray

--- src/main/markdown/buildpack-run.sh
+++ src/main/markdown/buildpack-run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh -x
-curl -L "https://dl.bintray.com/owlcms/owlcms-maven/app/owlcms/${project.artifactId}/${project.version}/owlcms-${project.version}.jar" -o owlcms.jar
+curl -L "https://dl.bintray.com/owlcms/owlcms-maven/app/owlcms/owlcms/${project.version}/owlcms-${project.version}.jar" -o owlcms.jar
