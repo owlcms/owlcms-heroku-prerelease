985cc61fdbffd333cd87da8daf2a3217e3d4ca33
Jean-François Lamy <jf@jflamy.dev>
2020-02-07 17:37:06 -0500

    4.4.6-beta2 heroku

--- update-owlcms4.cmd
+++ update-owlcms4.cmd
@@ -2,5 +2,5 @@
 set "appName=%HEROKU_APP%"
 set /p "appName=Enter path or just ENTER for default [%appName%] : "
 call heroku plugins:install plugin:builds
-call heroku builds:create --source-url https://github.com/jflamy-dev/owlcms4-heroku-prerelease/tree/4.4.6-beta1/owlcms4-heroku.tar.gz
+call heroku builds:create --source-url https://github.com/jflamy-dev/owlcms4-heroku-prerelease/releases/download/4.4.6-beta2/owlcms4-heroku.tar.gz --app %appName%
 pause
