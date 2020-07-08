7f44823aea82e2d279b22b86f96e0d33f402d960
Jean-François Lamy <jf@jflamy.dev>
2020-07-08 08:17:27 -0400

    sync

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh -x
-curl -L "https://dl.bintray.com/owlcms/owlcms-maven/app/owlcms/owlcms/4.8.0-beta2/owlcms-4.8.0-beta2.jar" -o owlcms.jar
+curl -L "https://dl.bintray.com/owlcms/owlcms-maven/app/owlcms/owlcms/4.8.0-SNAPSHOT/owlcms-4.8.0-SNAPSHOT.jar" -o owlcms.jar
