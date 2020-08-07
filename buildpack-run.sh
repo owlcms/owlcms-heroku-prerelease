4682617edf27d53352ede66a4d97a13c5f9edfe6
Jean-François Lamy <jf@jflamy.dev>
2020-08-07 00:39:18 -0400

    testing jdk11 mem settings

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh -x
-curl -L "https://dl.bintray.com/owlcms/owlcms-maven/app/owlcms/owlcms/4.9.1-SNAPSHOT/owlcms-4.9.1-SNAPSHOT.jar" -o owlcms.jar
+curl -L "https://dl.bintray.com/owlcms/owlcms-maven/app/owlcms/owlcms/4.9.1-rc04/owlcms-4.9.1-rc04.jar" -o owlcms.jar
