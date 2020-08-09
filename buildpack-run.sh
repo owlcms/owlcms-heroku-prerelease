608a9eba4e14e747bf218074753ac21c16718d46
Jean-François Lamy <jf@jflamy.dev>
2020-08-09 10:44:30 -0400

    Back to develop tags

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh -x
-curl -L "https://dl.bintray.com/owlcms/owlcms-maven/app/owlcms/owlcms/4.9.1/owlcms-4.9.1.jar" -o owlcms.jar
+curl -L "https://dl.bintray.com/owlcms/owlcms-maven/app/owlcms/owlcms/4.9.1-rc06/owlcms-4.9.1-rc06.jar" -o owlcms.jar
