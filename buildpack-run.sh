f2abdecef751a66c680f321ea1457c236b131679
Jean-François Lamy <jf@jflamy.dev>
2021-02-07 20:20:23 -0500

    inheriting locations from parent pom

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh -x
-curl -L "https://dl.bintray.com/owlcms/owlcms-maven/app/owlcms/owlcms/4.14.0-alpha01/owlcms-4.14.0-alpha01.jar" -o owlcms.jar
+curl -L "${owlcms-maven-url}/releases/app/owlcms/owlcms/4.14.0-alpha01/owlcms-4.14.0-alpha01.jar" -o owlcms.jar
