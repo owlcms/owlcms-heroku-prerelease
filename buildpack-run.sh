d8d435dc80a7a06bf972da7e5a4d18d1b7dd89e8
Jean-François Lamy <jf@jflamy.dev>
2021-02-08 02:39:40 +0000

    4.14.0-alpha04 heroku

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh -x
-curl -L "http://maven.owlcms.mywire.org:8088/releases/app/owlcms/owlcms/4.14.0-alpha03/owlcms-4.14.0-alpha03.jar" -o owlcms.jar
+curl -L "http://maven.owlcms.mywire.org:8088/releases/app/owlcms/owlcms/4.14.0-alpha04/owlcms-4.14.0-alpha04.jar" -o owlcms.jar
