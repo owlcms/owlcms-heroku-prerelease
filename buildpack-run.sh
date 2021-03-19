8449d66f9e9767c94340f2349f09aa5fea300096
Jean-François Lamy <jf@jflamy.dev>
2021-03-19 16:16:11 -0400

    4.15.5

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,6 +1,3 @@
 #!/bin/sh -x
-<<<<<<< HEAD
-curl -L "http://maven.owlcms.mywire.org:8088/releases/app/owlcms/owlcms/4.15.4/owlcms-4.15.4.jar" -o owlcms.jar
-=======
 curl -L "http://maven.owlcms.mywire.org:8088/releases/app/owlcms/owlcms/4.15.5-rc01/owlcms-4.15.5-rc01.jar" -o owlcms.jar
->>>>>>> origin/develop
+
