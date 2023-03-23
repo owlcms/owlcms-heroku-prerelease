6055c2b91401084af8d29608bd835dde4a63d297
Jean-François Lamy <lamyjeanfrancois@outlook.com>
2023-03-23 15:19:34 +0000

    38.2.0-rc01 heroku [skip ci]

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -5,4 +5,4 @@
 # Licensed under the Non-Profit Open Software License version 3.0  ("NPOSL-3.0")
 # License text at https://opensource.org/licenses/NPOSL-3.0
 #*******************************************************************************
-curl -L "http://maven.lerta.ca:8880/releases/app/owlcms/owlcms/38.1.0-rc02/owlcms-38.1.0-rc02.jar" -o owlcms.jar
+curl -L "http://maven.lerta.ca:8880/releases/app/owlcms/owlcms/38.2.0-rc01/owlcms-38.2.0-rc01.jar" -o owlcms.jar
